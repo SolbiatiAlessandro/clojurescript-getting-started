6f9682799992c9340bb86df21b06d4c1baf1a547
Alessandro Solbiati <lessandro@fb.com>
2020-05-26 00:47:57 +0100

    finished quick start

---
+++ README.md
@@ -0,0 +1,13 @@
+following https://clojurescript.org/guides/quick-start
+
+Some commands
+
+Compile
+```
+clj --main cljs.main --compile hello-world.core --repl
+```
+
+Compile for node
+```
+clj -m cljs.main --target node --output-to main.js -c hello-world.core
+```
---
+++ out/hello_world/core.js
@@ -0,0 +1,8 @@
+// Compiled by ClojureScript 1.10.758 {}
+goog.provide('hello_world.core');
+goog.require('cljs.core');
+goog.require('react_dom');
+hello_world.core.global$module$react_dom = goog.global["ReactDOM"];
+ReactDOM.render(React.createElement("h2",null,"Hello, react"),document.getElementById("app"));
+
+//# sourceMappingURL=core.js.map
